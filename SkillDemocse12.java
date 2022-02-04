1ebfc63db000faf555da4725c80c26d7669d4399
ajakhanwal <ajakhanwal@ucsd.edu>
2022-02-03 23:04:46 -0800

    lib + failing test

--- SkillDemocse12.java
+++ SkillDemocse12.java
@@ -1,1 +1,8 @@
-public class SkillDemocse12{}
+public class SkillDemocse12{
+    public static int factorial(int n){
+        int res = 0, i;
+        for (i=2; i<=n; i++)
+            res *= i;
+        return res;
+    }
+}
---
+++ SkillDemotest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemotest {
+    @Test
+    public void TestOne(){
+        assertEquals("factorial of 0 should be 1",1,SkillDemocse12.factorial(0));
+    }
+    
+}
