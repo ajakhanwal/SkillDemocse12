58ceebfdab77d504044f0f814aef8620cebb7f9d
ajakhanwal <ajakhanwal@ucsd.edu>
2022-02-03 23:08:29 -0800

    Changes for successful test

--- SkillDemocse12.java
+++ SkillDemocse12.java
@@ -1,6 +1,9 @@
 public class SkillDemocse12{
     public static int factorial(int n){
-        int res = 0, i;
+        //res is 1 such that the factorial
+        // never results in 0 and that it results 
+        //in 1 if we find the factorial of 0
+        int res = 1, i;
         for (i=2; i<=n; i++)
             res *= i;
         return res;
